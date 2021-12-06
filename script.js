ed639181f387df60595f32fc2b0b8f81c5223ae3
Calligrafixel <47078648+Calligrafixel@users.noreply.github.com>
2021-12-06 17:53:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ window.onload = function(){
     var _scroll = document.querySelector("#scroll");
 
     window.addEventListener('scroll', e=>{
-        var value = ( (((document.documentElement.scrollTop + window.innerHeight) / document.documentElement.scrollHeight) -0.16713)/(1-.16713) ).toFixed(3);
-        _scroll.innerHTML = (value * -16) + "rem";
+        var value = ( (((document.documentElement.scrollTop + window.innerHeight) / document.documentElement.scrollHeight) -0.16713) ).toFixed(3);
+        _scroll.innerHTML = Math.min(value/.656, 1);
 
         for(e of leftA){
-            e.style.left = ((1-value) * -16) + "rem";
+            e.style.left = ((Math.min(value/.656, 1)-1) * 16) + "rem";
         }
         for(e of rightA){
-            e.style.left = ((1-value) * 16) + "rem";
+            e.style.left = ((Math.min(value/.656, 1)-1) * -16) + "rem";
         }
     });
 }
